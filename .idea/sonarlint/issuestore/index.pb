db1dce91fbe9534451f2c40b744311eeabce87d9
Edgar Alfonso Aviles <edgar.alfonso.contractor@bbva.com>
2023-06-09 19:15:38 -0600

    Ejercicios Estructuras de control

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,11 +9,3 @@ b
 2src/com/cursoJava/tema1/arrays/EjemplosArrays.java,a\e\ae487968b2aea0c5d69d4684c2a03bc9d8d42028
 U
 %src/com/cursoJava/tema1/Scanners.java,2\3\239335faeeef841f1a56354596b8e5f6f4cd193c
-W
-'src/com/cursoJava/tema2/TiposDatos.java,e\e\ee8e4a5b29356e84adad1e2ffce4a8525c0ba292
-V
-&src/com/cursoJava/tema1/Funciones.java,e\3\e39750c2279a1d6b4aa3318f95d5309985de6735
-Q
-!src/com/cursoIntro/Funciones.java,0\1\013682f26ff48f1e823e9a1c21a8543fb89a368d
-U
-%src/com/cursoIntro/Condicionales.java,f\1\f1a9a6fd9ba0fcaae9a329520320066b8a3fe5e3
--- src/com/cursoIntro/Condicionales.java
+++ src/com/cursoIntro/Condicionales.java
@@ -1,5 +1,68 @@
 package com.cursoIntro;
 
-public class Condicionales {
+import com.cursoJava.tema1.control.Switch;
 
+public class Condicionales {
+    public static void main(String[] args) {
+        // IF
+        System.out.println("IF");
+        int numeroIf = 2;
+        if (numeroIf == 0) {
+            System.out.println("El numero es un cero");
+        } else if (numeroIf > 0) {
+            System.out.println("El numero es positivo");
+        } else {
+            System.out.println("El numero es negativo");
+        }
+
+
+        //While
+        System.out.println("While");
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+
+        //Do While
+        System.out.println("Do While");
+        int numeroDo =0;
+        do {
+            System.out.println(numeroDo);
+            numeroDo++;
+        } while (numeroDo > 3);
+
+        //FOR
+        System.out.println("For");
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+
+        //Switch
+        System.out.println("Switch");
+        String estacion = "Verano";
+
+        switch (estacion) {
+            case "Primavera":{
+                System.out.println("Es primavera");
+                break;
+            }
+            case "Verano":{
+                System.out.println("Es verano");
+                break;
+            }
+            case "Otono":{
+                System.out.println("Es otono");
+                break;
+            }
+            case "Inviero":{
+                System.out.println("Es inviero");
+                break;
+            }
+            default:{
+                System.out.println("Esa estacion no existe");
+            }
+        }
+
+    }
 }
