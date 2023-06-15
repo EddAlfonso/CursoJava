4e70f511b88f23b96fedbb5f8dd949db85d8693c
Edgar Alfonso Aviles <edgar.alfonso.contractor@bbva.com>
2023-06-15 16:44:17 -0600

    Ejercicio encapsulamiento

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+V
+&src/com/cursoJava/tema1/Funciones.java,e\3\e39750c2279a1d6b4aa3318f95d5309985de6735
+W
+'src/com/cursoJava/tema2/TiposDatos.java,e\e\ee8e4a5b29356e84adad1e2ffce4a8525c0ba292
+Q
+!src/com/cursoIntro/Funciones.java,0\1\013682f26ff48f1e823e9a1c21a8543fb89a368d
---
+++ src/com/cursoIntro/Encapsulamiento.java
@@ -0,0 +1,49 @@
+package com.cursoIntro;
+
+public class Encapsulamiento {
+    public static void main(String[] args) {
+
+        // 4. Crear objeto Persona
+        Personsa persona1 = new Personsa();
+
+        //5. Dar valores y mostrarlos
+        persona1.setNombre("Edgar");
+        persona1.setEdad(28);
+        persona1.setTelefono(1122339900);
+
+        System.out.println("Nombre:" + persona1.getNombre() + " Edad:" + persona1.getEdad() + " Telefono:" + persona1.getTelefono());
+    }
+}
+// 1. Crear clase Persona
+class Personsa{
+    //2. Variables
+    private int edad;
+    private String nombre;
+    private long telefono;
+
+    //3. Getters and setters
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+}
